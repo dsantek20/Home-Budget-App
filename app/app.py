f61856da47764e44df024cd1b170278e10ff499a
Domagoj Šantek <domagoj.santek@htech.hr>
2025-11-13 11:44:07 +0100

    feat: added logger and middleware

--- app/app.py
+++ app/app.py
@@ -1,8 +1,28 @@
+from contextlib import asynccontextmanager
+import logging
 from fastapi import FastAPI, HTTPException
 from fastapi.exceptions import RequestValidationError
+from app_config import get_app_config
+from common.logger import initialize_logging
 from error_handling.error_handling import ApplicationException, application_exception_handler, exception_handler, http_exception_handler, runtime_error_handler, validation_exception_handler
+from common.middleware import CorrelationIDMiddleware
 
-app = FastAPI()
+config = get_app_config()
+log = logging.getLogger(__name__)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    app.state.config = config
+    initialize_logging(config)
+    log.info(f"{config.app_name} service started")
+    
+    yield 
+
+    log.info(f"{app.state.config.app_name} service shutdown")
+
+app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(CorrelationIDMiddleware)
 
 # Define error handling
 app.add_exception_handler(ApplicationException, application_exception_handler)
---
+++ app/app_config.py
@@ -0,0 +1,26 @@
+import logging
+from functools import lru_cache
+from typing import Annotated
+from fastapi import Depends
+from pydantic.v1 import BaseSettings
+
+log = logging.getLogger(__name__)
+
+
+class AppConfiguration(BaseSettings):
+    app_name: str = "Home Budget"
+    debug: bool = False
+    log_file_path: str
+    log_file_name: str
+
+    class Config:
+        file = f".env"
+        log.info("Config file used: " + file)
+        env_file = file
+
+
+@lru_cache
+def get_app_config() -> AppConfiguration:
+    return AppConfiguration()
+
+AppConfig = Annotated[AppConfiguration, Depends(get_app_config)]
