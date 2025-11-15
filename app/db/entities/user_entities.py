674301754194d2878b77b08f1a00725b30275319
Domagoj Šantek <domagoj.santek@htech.hr>
2025-11-15 11:54:04 +0100

    feat: added category entity

--- app/db/entities/user_entities.py
+++ app/db/entities/user_entities.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, String
 from db.entities.base_model import BaseUUIDModel
-
+from sqlalchemy.orm import relationship
 
 class User(BaseUUIDModel):
     __tablename__ = "user"
@@ -10,3 +10,10 @@ class User(BaseUUIDModel):
     password = Column(String, nullable=False)
     first_name = Column(String, nullable=True)
     last_name = Column(String, nullable=True)
+
+    categories = relationship(
+        "Category",
+        back_populates="user",
+        lazy="selectin",
+        cascade="all, delete-orphan",
+    )
