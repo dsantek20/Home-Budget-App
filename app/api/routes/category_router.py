5aef4ed235909b66d2e4c1dee9fc391d776ada54
Domagoj Šantek <domagoj.santek@htech.hr>
2025-11-15 14:09:58 +0100

    feat: get custom categories

--- app/api/routes/category_router.py
+++ app/api/routes/category_router.py
@@ -12,6 +12,10 @@ category_router = APIRouter(dependencies=[Depends(get_current_user)])
 async def get_predefined_categories(service: CategoryServiceInstance):
     return await service.get_predefined_categories()
 
-@category_router.post("/create", response_model=CategoryGet)
+@category_router.get("/custom", response_model=List[CategoryGet])
+async def get_user_custom_categories(current_user: CurrentUser):
+    return [CategoryGet.model_validate(category) for category in current_user.categories]
+
+@category_router.post("/", response_model=CategoryGet)
 async def create_new_category(current_user: CurrentUser, request: CategoryRequest, service: CategoryServiceInstance):
     return await service.create_new_category(current_user, request)
