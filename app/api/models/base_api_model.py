70d8a5105e3390847c25b2cb17afb191ac39e539
Domagoj Šantek <domagoj.santek@htech.hr>
2025-11-16 19:38:04 +0100

    feat: added full tests for category routes

--- app/api/models/base_api_model.py
+++ app/api/models/base_api_model.py
@@ -1,7 +1,6 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 class BaseApiModel(BaseModel):
-
-    class Config:
-        from_attributes = True
-
+    model_config = ConfigDict(
+        from_attributes=True,
+    )
