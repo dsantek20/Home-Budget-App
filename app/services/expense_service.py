231a990911d2cad8b64e84a1d679bee26b302b2f
Domagoj Šantek <domagoj.santek@htech.hr>
2025-11-17 15:52:06 +0100

    feat: check category type in expense

--- app/services/expense_service.py
+++ app/services/expense_service.py
@@ -1,9 +1,11 @@
 
-from decimal import Decimal
 from typing import Annotated, List
 from uuid import UUID
 from fastapi import Depends, Response, status
 from api.models.expense_models import ExpenseCreate, ExpenseGet, ExpenseUpdate, ExpenseFilter
+from db.entities.category_entities import Category
+from db.entities.types.category_type import CategoryType
+from error_handling.error_handling import ApplicationException
 from db.entities.user_entities import User
 from db.entities.expense_entities import Expense
 from db.dao.expense_dao import ExpenseDao, ExpenseDaoInstance
@@ -22,6 +24,15 @@ class ExpenseService:
         return [ExpenseGet.model_validate(expense) for expense in expenses]
     
     async def create_new_expense(self, current_user: User, request: ExpenseCreate) -> ExpenseGet:
+        category = await self.dao.get_by_id(Category, request.category_id)
+    
+        if category.category_type != CategoryType.EXPENSE.value:
+            raise ApplicationException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                message="Invalid category type",
+                debug_message=f"Category must be of type EXPENSE, but got {category.category_type}",
+                code="SVC-4002"
+            )
         expense = await self.dao.create(Expense, **request.model_dump(), user_id=current_user.id)
         if expense:
             current_user.balance = current_user.balance - request.amount
@@ -29,6 +40,16 @@ class ExpenseService:
         return ExpenseGet.model_validate(expense)
     
     async def update_expense(self, current_user: User, expense_id: UUID, request: ExpenseUpdate) -> ExpenseGet:
+        if request.category_id:
+            category = await self.dao.get_by_id(Category, request.category_id)
+        
+            if category.category_type != CategoryType.EXPENSE.value:
+                raise ApplicationException(
+                    status_code=status.HTTP_400_BAD_REQUEST,
+                    message="Invalid category type",
+                    debug_message=f"Category must be of type EXPENSE, but got {category.category_type}",
+                    code="SVC-4002"
+                )
         expense = await self.dao.get_by_id(Expense, expense_id)
         old_amount = expense.amount
         expense_updated = await self.dao.update_expense(expense, request)
